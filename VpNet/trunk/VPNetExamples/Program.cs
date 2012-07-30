bef8e071281979661fd536f07913cecbb42c4ed6
SND\cube3_cp <SND\cube3_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-07-30 14:58:21 +0000

    Added Bot example, hello world!
    git-svn-id: https://vpnet.svn.codeplex.com/svn@93343 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ VpNet/trunk/VPNetExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Configuration;
+using VpNet.Core;
+
+namespace VPNetExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+            // connect to universe.
+                var instance = new Instance();
+                instance.Connect(ConfigurationManager.AppSettings["server"], ushort.Parse(ConfigurationManager.AppSettings["serverPort"]));
+                instance.Login(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"], ConfigurationManager.AppSettings["botName"]);
+                instance.Enter(ConfigurationManager.AppSettings["world"]);
+                instance.Say("Hello World!");
+                instance.Wait(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Have you entered your credentials in the app.config file?");
+            }
+            Console.ReadLine();
+        }
+    }
+}
