095538e8dac77894224002633f77f2f3f1b22736
SND\cube3_cp <SND\cube3_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-08-08 00:01:21 +0000

    git-svn-id: https://vpnet.svn.codeplex.com/svn@93594 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ VpNet/branches/VpNetWcf/VpNetFramework/BotEngineConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using VpNet.Core.Structs;
+
+namespace VpNetFramework
+{
+    public struct BotEngineConfiguration
+    {
+        public string Server { get;set; }
+        public ushort ServerPort { get; set; }
+        public string LoginUser { get; set; }
+        public string LoginPassword { get; set; }
+        public string BotName { get; set; }
+        public string World { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public ReconnectSettings ReconnectUniverse { get; set; }
+        public ReconnectSettings ReconnecWorld { get; set; }
+    }
+}
