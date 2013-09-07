fa604f182a07d27bc77285c8d5a9cf799076e034
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2013-09-07 07:00:47 +0100

    - internalized DateTime extensions
    - added UnixEpoch

--- VPNet/Extensions.cs
+++ VPNet/Extensions.cs
@@ -4,15 +4,17 @@ namespace VP
 {
     internal static class DateTimeExt
     {
-        public static int ToUnixTimestampUTC(this DateTime date)
+        internal static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        internal static int ToUnixTimestampUTC(this DateTime date)
         {
-            return (int) DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))
+            return (int) DateTime.UtcNow.Subtract(UnixEpoch)
                 .TotalSeconds;
         }
 
-        public static DateTime FromUnixTimestampUTC(int timestamp)
+        internal static DateTime FromUnixTimestampUTC(int timestamp)
         {
-            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(timestamp);
+            return UnixEpoch.AddSeconds(timestamp);
         }
     }
 }
