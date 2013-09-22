6724daf1fe23b2f0f0db0bf838ce14dc6814ad74
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2013-09-22 10:49:39 +0100

    - added r1014 CentOS linux native library
    - created nuspec file
    - made DLLHandler multi-platformish
    - added basic bot template project (unbuilt; to be used for end-users)
    - moved native libraries into VPNet folder
    - cleaned up unused platforms in Solution

--- VPNet/Native/Functions.cs
+++ VPNet/Native/Functions.cs
@@ -19,50 +19,50 @@ namespace VP.Native
                 throw new VPException( (ReasonCode) rc );
         }
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_init(int build);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern IntPtr vp_create();
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_destroy(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_connect_universe(IntPtr instance,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string host, 
             int port);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_login(IntPtr instance,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string username,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string password,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string botname);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_wait(IntPtr instance, int time);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_enter(IntPtr instance,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string worldname);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_leave(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_say(IntPtr instance,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string message);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention=CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention=CallingConvention.Cdecl)]
         internal static extern int vp_console_message(IntPtr instance, int session,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string name,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string message,
             int effects, byte red, byte green, byte blue);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_event_set(IntPtr instance, int eventName, [MarshalAs(UnmanagedType.FunctionPtr)]EventDelegate eventFunction);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_callback_set(IntPtr instance, int callbackName, [MarshalAs(UnmanagedType.FunctionPtr)]CallbackDelegate callbackFunction);
 
         // Unused
@@ -73,58 +73,58 @@ namespace VP.Native
         //[DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
         //internal static extern void vp_user_data_set(IntPtr instance, IntPtr data);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_state_change(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_int(IntPtr instance, [MarshalAs(UnmanagedType.I4)]IntAttributes name);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern float vp_float(IntPtr instance, [MarshalAs(UnmanagedType.I4)]FloatAttributes name);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToManaged))]
         internal static extern string vp_string(IntPtr instance, [MarshalAs(UnmanagedType.I4)]StringAttributes name);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern IntPtr vp_data(IntPtr instance, [MarshalAs(UnmanagedType.I4)]DataAttributes name, out int length);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_int_set(IntPtr instance, [MarshalAs(UnmanagedType.I4)]IntAttributes name, int value);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_float_set(IntPtr instance, [MarshalAs(UnmanagedType.I4)]FloatAttributes name, float value);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void vp_string_set(IntPtr instance, 
             [MarshalAs(UnmanagedType.I4)] StringAttributes name,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string value);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_data_set(IntPtr instance, [MarshalAs(UnmanagedType.I4)]DataAttributes name, int length, IntPtr data);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_query_cell(IntPtr instance, int x, int z);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_object_add(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_object_change(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_object_delete(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_object_click(IntPtr instance);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_world_list(IntPtr instance, int time);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_user_attributes_by_id(IntPtr instance, int userId);
         
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_user_attributes_by_name(IntPtr instance,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8StringToNative))] string name);
 
@@ -141,16 +141,16 @@ namespace VP.Native
         //[DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
         //internal static extern int vp_friend_delete(IntPtr instance, int friendId);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_terrain_query(IntPtr instance, int tile_x, int tile_z, int[,] nodes);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_terrain_node_set(IntPtr instance, int tile_x, int tile_z, int node_x, int node_z, byte[] node);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_avatar_click(IntPtr instance, int session);
 
-        [DllImport(DLLHandler.VPDLL, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(DLLHandler.dllBase, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int vp_teleport_avatar(
             IntPtr instance,
             int session,
