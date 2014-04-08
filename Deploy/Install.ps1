06c06da8c62eb9ad5b80ea8458b2d171055ccb29
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2014-04-08 16:22:40 +0100

    - fixed deployment and nuspec
    - added install script

---
+++ Deploy/Install.ps1
@@ -0,0 +1,24 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function MarkDirectoryAsCopyToOutputRecursive($item)
+{
+    $item.ProjectItems | ForEach-Object { MarkFileASCopyToOutputDirectory($_) }
+}
+
+function MarkFileASCopyToOutputDirectory($item)
+{
+    Try
+    {
+        $item.Properties.Item("CopyToOutputDirectory").Value = 1
+		Write-Host Attached native binary $item.Name as "Copy always" item
+    }
+    Catch
+    {
+        MarkDirectoryAsCopyToOutputRecursive($item)
+    }
+}
+
+# Now mark everything in the a directory as "Copy to newer"
+# http://stackoverflow.com/questions/8474253/nuget-how-can-i-change-property-of-files-with-install-ps1-file
+MarkDirectoryAsCopyToOutputRecursive($project.ProjectItems.Item("x64"))
+MarkDirectoryAsCopyToOutputRecursive($project.ProjectItems.Item("x86"))
